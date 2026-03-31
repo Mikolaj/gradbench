59c062d0bf85585be5d194f4f581e6a68ff4a21d
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2026-03-31 22:50:12 +0200

    Speed up tools/horde-ad/src/GradBench/GD.hs in the most obvious way

--- tools/horde-ad/src/GradBench/GD.hs
+++ tools/horde-ad/src/GradBench/GD.hs
@@ -21,18 +21,15 @@ multivariateArgmin
 {-# INLINE multivariateArgmin #-}
 multivariateArgmin magnitude_squared scale fg x0 = loop (x0, fx0, gx0, 1e-5, 0)
  where
-  magnitude :: target y -> target (TKScalar a)
-  magnitude = sqrt . magnitude_squared
+  margin_squared = 1e-5 * 1e-5
   distance_squared :: target y -> target y -> target (TKScalar a)
   distance_squared u v = magnitude_squared (u - v)
-  distance :: target y -> target y -> target (TKScalar a)
-  distance u v = sqrt $ distance_squared u v
   (fx0, gx0) = fg x0
   loop :: (target y, target (TKScalar a), target y, a, Int) -> target y
   loop (x, fx, gx, eta, i)
-    | magnitude gx <= 1e-5 = x
+    | magnitude_squared gx <= margin_squared = x
     | i == 10 = loop (x, fx, gx, 2 * eta, 0)
-    | distance x x_prime <= 1e-5 = x
+    | distance_squared x x_prime <= margin_squared = x
     | fx_prime < fx = loop (x_prime, fx_prime, gx_prime, eta, i + 1)
     | otherwise = loop (x, fx, gx, eta / 2, 0)
    where
